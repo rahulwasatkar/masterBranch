1df50073f31e96bacdea585b08d095b4fb055027
rahul wasatkar <rswasatkar25@gmail.com>
2022-12-11 20:40:32 +0530

    1st commit

---
+++ github.com/rahulwasatkar/masterBranch.git/mainBranch/src/main/java/com/rahul/MainBranchApplication.java
@@ -0,0 +1,13 @@
+package com.rahul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainBranchApplication.class, args);
+	}
+
+}
